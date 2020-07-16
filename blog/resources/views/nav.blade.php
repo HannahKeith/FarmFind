23d3f395b31112562a6e1dde719ce66626cebd2e
Hannah Keith <hannahkeith@Hannahs-Air.attlocal.net>
2020-07-16 13:12:30 -0400

    bootstrap nav added to pages

--- blog/resources/views/nav.blade.php
+++ blog/resources/views/nav.blade.php
@@ -1,6 +1,37 @@
+<!--
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-<nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="/">Home</a>
-  <a class="navbar-brand" href="/meat">Meat</a>
-  <a class="navbar-brand" href="/veg">Vegetables</a>
+
+
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+      <a class="" href="/">Home</a>
+    </li>
+    <li class="nav-item active">
+      <a class="" href="/meat">Meat</a>
+    </li>
+    <li>
+      <a class="" href="/veg">Vegetables</a>
+    </li>
+  </ul> -->
+
+
+
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="/">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="/veg">Vegetables</a>
+      <a class="nav-item nav-link" href="/meat">Meat</a>
+    
+    </div>
+  </div>
+</nav>
+
+
 </nav>
--- blog/resources/views/veg.blade.php
+++ blog/resources/views/veg.blade.php
@@ -4,12 +4,15 @@
   <head>
     <meta charset="utf-8">
     <title>Vegetables</title>
+
+    <link rel="stylesheet" href="./resources/sass/app.scss">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
   </head>
   <body>
-
-    @include('nav')
-
-    <h1>This is the Vegatables directory</h1>
+      @include('nav')
+    <h1>This is the Vegetables directory</h1>
 
   </body>
 </html>
