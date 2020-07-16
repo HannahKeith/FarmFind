68c13540516aefce01e53bc839c423e0f89b58ee
Hannah Keith <hannahkeith@Hannahs-Air.attlocal.net>
2020-07-16 11:13:19 -0400

    meat and veg routes set up

---
+++ blog/resources/views/veg.blade.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Vegetables</title>
+  </head>
+  <body>
+
+    <h1>This is the Vegatables directory</h1>
+
+  </body>
+</html>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('veg', function () {
+    return view('veg');
+});
+
+Route::get('meat', function () {
+    return view('meat');
+});
