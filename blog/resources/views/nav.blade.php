6b706409ac958059c3fb65923067d51c464f4425
Hannah Keith <hannahkeith@Hannahs-Air.attlocal.net>
2020-07-16 13:28:07 -0400

    added bootstrap to nav

--- blog/resources/views/nav.blade.php
+++ blog/resources/views/nav.blade.php
@@ -1,24 +1,9 @@
-<!--
+
+
+
+
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-
-
-
-  <ul class="navbar-nav mr-auto">
-    <li class="nav-item active">
-      <a class="" href="/">Home</a>
-    </li>
-    <li class="nav-item active">
-      <a class="" href="/meat">Meat</a>
-    </li>
-    <li>
-      <a class="" href="/veg">Vegetables</a>
-    </li>
-  </ul> -->
-
-
-
-
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -28,10 +13,10 @@
       <a class="nav-item nav-link active" href="/">Home <span class="sr-only">(current)</span></a>
       <a class="nav-item nav-link" href="/veg">Vegetables</a>
       <a class="nav-item nav-link" href="/meat">Meat</a>
-    
+
     </div>
   </div>
 </nav>
 
 
-</nav>
+</nav> 
